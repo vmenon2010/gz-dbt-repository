ba2b43bc2b97c9d0cd6b170c3b7652d1a736b95b
vmenon10@gmail.com <vmenon10@gmail.com>
2024-04-17 11:35:48 +0000

    added orders margin table

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -4,7 +4,7 @@ SELECT
     orders_id,
     revenue, 
     quantity, 
-    CAST(purchase_price AS FLOAT64), 
+    CAST(purchase_price AS FLOAT64) AS purchase_price, 
     ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
     s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
 FROM {{ref("stg_raw__sales")}} s
